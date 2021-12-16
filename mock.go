21d08167c002d0b25ef8974ce5213fe71fa90864
Evan White <evanwht@stryd.com>
2021-12-16 10:33:25 -0700

    concurrent safe mock client (#5)

--- mock.go
+++ mock.go
@@ -5,10 +5,12 @@ import (
 	"crypto/md5"
 	"encoding/json"
 	"fmt"
+	"sync"
 )
 
 // MockClient Mocked version of Mixpanel intended for unit tests only
 type MockClient struct {
+	mu         sync.Mutex
 	eventsByID map[string][]Event
 }
 
@@ -19,11 +21,15 @@ func NewMockClient() *MockClient {
 }
 
 func (m *MockClient) Track(e Event) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	m.eventsByID[e.DistinctID] = append(m.eventsByID[e.DistinctID], e)
 	return nil
 }
 
 func (m *MockClient) Import(es []Event) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	for _, e := range es {
 		if len(e.InsertID) == 0 {
 			f := md5.New()
@@ -36,6 +42,8 @@ func (m *MockClient) Import(es []Event) error {
 }
 
 func (m *MockClient) String() string {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	var buf bytes.Buffer
 	enc := json.NewEncoder(&buf)
 	enc.SetIndent("", "\t")
